088bed2badf716c719387aecba8bf9432ba7336c
Laura T <68660183+Tuzys@users.noreply.github.com>
2024-05-03 16:26:39 +0300

    removed del, create n update. changed controllers

--- controllers/parts.js
+++ controllers/parts.js
@@ -1,18 +1,22 @@
 import express from "express";
-import { varuosadData } from "../data/data.js";
 import { v4 as uuidv4 } from "uuid";
+import fs from "fs";
+import csv from "csv-parser";
 
-let parts = [...varuosadData];
+let parts = [];
 
-export const createPart = (req, res) => {
-  const part = req.body;
-
-  parts.push({ ...part, Id: uuidv4() });
-
-  res.send(`Part with the name ${part.Nimi} added to the database!`);
+const renderParts = () => {
+  fs.createReadStream("data/LE.csv")
+    .pipe(csv())
+    .on("data", (data) => {
+      parts.push(data);
+    });
 };
 
-export const getParts = (req, res) => {
+
+
+export const getParts = async (req, res) => {
+  await renderParts();
   res.send(parts);
 };
 
@@ -22,33 +26,4 @@ export const getPart = (req, res) => {
   const foundPart = parts.find(part => part.Id === Id);
 
   res.send(foundPart);
-};
-
-export const deletePart = (req, res) => {
-  const { Id } = req.params;
-
-  parts = parts.filter(part => part.Id != Id);
-
-  res.send(`Part with the Id ${Id} deleted from the database.`);
-};
-
-export const updatePart = (req, res) => {
-  const { Id } = req.params;
-
-  const { Nimi, l1, l2, l3, l4, l5, sus1, sus2, Tootja, hind } = req.body;
-
-  const part = parts.find(part => part.Id === Id);
-
-  if (Nimi) part.Nimi = Nimi;
-  if (l1) part.l1 = l1;
-  if (l2) part.l2 = l2;
-  if (l3) part.l3 = l3;
-  if (l4) part.l4 = l4;
-  if (l5) part.l5 = l5;
-  if (sus1) part.sus1 = sus1;
-  if (sus2) part.sus2 = sus2;
-  if (Tootja) part.Tootja = Tootja;
-  if (hind) part.hind = hind;
-
-  res.send(`Part with the Id ${Id} has been updated`);
 };
--- routes/varuosad.js
+++ routes/varuosad.js
@@ -1,16 +1,11 @@
 import express from "express";
-import { getParts, createPart, getPart, deletePart, updatePart } from "../controllers/parts.js";
+import { getParts, getPart } from "../controllers/parts.js";
 
 const router = express.Router();
 
 router.get("/", getParts);
 
-router.post("/", createPart);
 
 router.get("/:Id", getPart);
-
-router.delete("/:Id", deletePart);
-
-router.patch("/:Id", updatePart);
 
 export default router;
